c47222fffb443041530649cbd7119aec82bdbdb4
Paulo Albuquerque <paulora2405@gmail.com>
2024-08-05 16:21:23 -0300

    Add number of moves to POI mobility

--- edge_sim_py/components/mobility_models/point_of_interest_mobility.py
+++ edge_sim_py/components/mobility_models/point_of_interest_mobility.py
@@ -14,18 +14,37 @@ def point_of_interest_mobility(user: User):
     if user.point_of_interest is None:
         return
 
-    _parameters = getattr(user, "mobility_model_parameters", {})
-    (x1, y1) = user.coordinates_trace[-1]
-    (x2, y2) = user.point_of_interest.coordinates
-    total_distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    parameters = getattr(user, "mobility_model_parameters", {})
+    # Number of "mobility routines" added each time the method is called. Defaults to 1.
+    n_moves = parameters["n_moves"] if "n_moves" in parameters else 1
 
-    if total_distance <= user.movement_distance:
-        user.coordinates_trace.extend((x2, y2))
-        user.coordinates = (x2, y2)
-        return
+    mobility_path = []
 
-    ratio = user.movement_distance / total_distance
-    x3 = x1 + (x2 - x1) * ratio
-    y3 = y1 + (y2 - y1) * ratio
-    user.coordinates_trace.append((x3, y3))
-    user.coordinates = (x3, y3)
+    for _ in range(n_moves):
+        (x1, y1) = user.coordinates
+        (x2, y2) = user.point_of_interest.coordinates
+        (x3, y3) = (0, 0)
+        total_distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+        if total_distance <= user.movement_distance:
+            x3 = x2
+            y3 = y2
+        else:
+            ratio = user.movement_distance / total_distance
+            x3 = x1 + (x2 - x1) * ratio
+            y3 = y1 + (y2 - y1) * ratio
+
+        mobility_path.append((x3, y3))
+
+    # # We assume that users do not necessarily move from one step to another, as one step may represent a very small time interval
+    # # (e.g., 1 millisecond). Therefore, each position on the mobility path is repeated N times, so that user takes a predefined
+    # # amount of time steps to move from one position to another. By default, users take at least 60 seconds to move across positions
+    # # in the map. This parameter can be changed by passing a "seconds_to_move" key to the "parameters" parameter.
+    # if "seconds_to_move" in parameters and type(parameters["seconds_to_move"]) is int and parameters["seconds_to_move"] < 1:
+    #     raise Exception("The 'seconds_to_move' key passed inside the mobility model's 'parameters' attribute must be > 1.")
+    # seconds_to_move = parameters["seconds_to_move"] if "seconds_to_move" in parameters else 60
+    # mobility_path = [item for item in mobility_path for _ in range(int(seconds_to_move / user.model.tick_duration))]
+
+    # Adding the path that connects the current to the target location to the client's mobility trace
+    user.coordinates_trace.extend([loc.coordinates for loc in mobility_path])
+    user.coordinates = user.coordinates_trace[-1]
